20f136481200cb94d3e2d69219ff321c1f7adfac
Alexander Rafferty <alexanderrafferty@Alexanders-MacBook-Pro.local>
2026-08-19 11:21:48 +1000

    add consistency checks

--- src/bit_permutation.rs
+++ src/bit_permutation.rs
@@ -132,6 +132,10 @@ impl PartialBits {
     pub fn max_bits(self) -> u64 {
         self.bits
     }
+
+    pub fn contains(self, value: u64) -> bool {
+        (value | !self.used) == self.bits
+    }
 }
 
 impl BitOr for PartialBits {
@@ -942,9 +946,12 @@ mod test {
         let a = PartialBits::full(0b000_111_111).with_used(0b111_111_000);
         let b = PartialBits::full(0b011_011_011).with_used(0b110_110_110);
         let c = PartialBits::full(0b011_111_111).with_used(0b110_111_010);
-        const X: u16 = 510;
-        const Y: u16 = 442;
         assert_eq!(a | b, c);
+
+        assert!(c.contains(a.min_bits() | b.min_bits()));
+        assert!(c.contains(a.min_bits() | b.max_bits()));
+        assert!(c.contains(a.max_bits() | b.min_bits()));
+        assert!(c.contains(a.max_bits() | b.max_bits()));
     }
 
     #[test]
@@ -953,5 +960,10 @@ mod test {
         let b = PartialBits::full(0b011_011_011).with_used(0b110_110_110);
         let c = PartialBits::full(0b000_011_011).with_used(0b111_110_100);
         assert_eq!(a & b, c);
+
+        assert!(c.contains(a.min_bits() & b.min_bits()));
+        assert!(c.contains(a.min_bits() & b.max_bits()));
+        assert!(c.contains(a.max_bits() & b.min_bits()));
+        assert!(c.contains(a.max_bits() & b.max_bits()));
     }
 }
